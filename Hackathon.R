baa4669076588053563a12c7266bb5fa1c002bd5
louis <louis.atallah@icloud.com>
2024-11-15 18:54:45 -0700

    wip

--- Hackathon.R
+++ Hackathon.R
@@ -11,16 +11,17 @@ plot(pca, type = "l")
 biplot(pca)
 
 library(factoextra)
-fviz_pca_var(pca,
-             col.var = "contrib", 
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE     
+fviz_pca_var(
+  pca,
+  col.var = "contrib", 
+  gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+  repel = TRUE     
 )
 fviz_contrib(pca, choice = "var", axes = 1)
 
 # loudness - energy - acousticness
 
-library(caTools) # install it first in the console
+library(caTools) 
 set.seed(703185)
 split = sample.split(data_train, SplitRatio = 0.70)
 training_set = subset(data_train, split == TRUE)
@@ -31,16 +32,16 @@ prediction_accuracy = function(pred,actual){
   mean(pred==actual)
 }
 
-#logistic 0.615
+#logistic 
 model_log = glm(target~loudness+energy+acousticness, data = training_set)
 summary(model_log)
+
 pred.log = predict(model_log, newdata = test_set, type = "response")
 pred.log = ifelse(pred.log >=0.5,1,0)
 prediction_accuracy(pred.log, test_set$target)
 
 prediction.log = predict(model_log, newdata = test)
 prediction.log = ifelse(prediction.log >=0.5,1,0)
-
 
 to_be_submitted = data.frame(id=rownames(test) , target=prediction.log)
 write.csv(to_be_submitted , file = "nouveau.csv", row.names = F)
@@ -49,8 +50,6 @@ head(df)
 
 #tree
 library(rpart)
-
-
 model_tree = rpart(target~loudness+energy+acousticness,method = "class" ,data = training_set)
 pred.tree = predict(model_tree, newdata = test_set, type= "class")
 prediction_accuracy(pred.tree, test_set$target)
@@ -60,7 +59,7 @@ prediction.tree = predict(model_tree, newdata = test, type = "class")
 to_be_submitted = data.frame(id=rownames(test) , target=prediction.tree)
 write.csv(to_be_submitted , file = "nouveau.csv", row.names = F)
 
-#Bagging meilleur 0.908
+#Bagging meilleur 
 library(randomForest)
 model_bagging = randomForest(target~., data = training_set, mtry = 13 , importance = TRUE, ntrees = 10000, method = "class")
 pred.bagging = predict(model_bagging, newdata = test_set)
@@ -73,7 +72,7 @@ prediction.bagging = ifelse(prediction.bagging>=0.5,1,0)
 to_be_submitted = data.frame(id=rownames(test) , target=prediction.bagging)
 write.csv(to_be_submitted , file = "nouveau.csv", row.names = F)
 
-#Random Forests 2ème meilleur 0.909
+#Random Forests 
 model_rf = randomForest(target~., data = training_set, mtry = 3, importance =TRUE, ntrees = 10000, type = "class")
 pred.rf = predict(model_rf, newdata = test_set)
 pred.rf = ifelse(pred.rf >=0.5,1,0)
@@ -85,11 +84,16 @@ prediction.rf = ifelse(prediction.rf >=0.5,1,0)
 to_be_submitted = data.frame(id=rownames(test) , target=prediction.rf)
 write.csv(to_be_submitted , file = "nouveau.csv", row.names = F)
 
-#Boosting 0.909
+#Boosting 
 library(gbm)
-
-model_boosting = gbm(target~., data = training_set, distribution = "bernoulli", 
-                     n.trees = 5000, interaction.depth = 10, shrinkage = 0.01)
+model_boosting = gbm(
+  target~., 
+  data = training_set, 
+  distribution = "bernoulli", 
+  n.trees = 5000, 
+  interaction.depth = 10, 
+  shrinkage = 0.01
+)
 pred.boosting = predict(model_boosting, newdata = test_set, type = "response")
 pred.boosting = ifelse(pred.boosting>=0.5,1,0)
 prediction_accuracy(pred.boosting, test_set$target)
@@ -100,15 +104,13 @@ prediction.boosting = ifelse(prediction.boosting >=0.5,1,0)
 to_be_submitted = data.frame(id=rownames(test) , target=prediction.boosting)
 write.csv(to_be_submitted , file = "nouveau.csv", row.names = F)
 
-#lda 0.667
-
+#lda 
 library(MASS)
-
 model_lda = lda(target~., data = training_set)
 pred.lda = predict(model_lda, newdata = test_set, type = "response")
 prediction_accuracy(pred.lda$class,test_set$target)
 
-#qda 0.714
+#qda 
 model_qda = qda(target~., data = training_set)
 pred.qda = predict(model_qda, newdata = test_set, type = "response")
 prediction_accuracy(pred.qda$class,test_set$target)
